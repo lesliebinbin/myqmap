2c5981bd345b30bfbdad82fe692a1b9dd35f10e0
lesliebinbin <506120340@qq.com>
2019-03-29 00:40:12 +1000

    project init

---
+++ back-end/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to back-end
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ back-end/test/back_end/core_test.clj
@@ -0,0 +1,7 @@
+(ns back-end.core-test
+  (:require [clojure.test :refer :all]
+            [back-end.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
